0530dee858a63a2d50fa9cee84d937b5f066d3c9
mohamedaliazouz <mohamedaliazouz@18292c93-4abe-417d-cb20-25aa85239c0b>
2012-01-18 10:09:49 +0000

    upload files to the server

---
+++ views/js/data.js
@@ -0,0 +1,29 @@
+YallakoraStaticData=function(){
+    var yallakoraStaticData={
+        "MatchListURL":"http://www.yallakora.com/Services/AjaxMethods.asmx/GetMatchesListByRegion",
+        "Days":[
+            {
+                "name":"Yesterday",
+                "value":0
+            },
+            {
+                "name":"Today",
+                "value":1
+            },
+            {
+                "name":"Tomorrow",
+                "value":2
+            }
+        ],
+        "TeamLogoURL":"http://www.yallakora.com/pictures/TeamLogo/thumb/",
+        "TeamUrl":"http://www.yallakora.com/arabic/ykchampions/team.aspx",
+        "LeagueLogo":"http://www.yallakora.com/pictures/ChampLogos/",
+        "LeagueURL":"http://www.yallakora.com/arabic/ykchampions/default.aspx?region=&TourId="
+    }
+    $(function(){
+
+    })
+    return yallakoraStaticData;
+}
+
+var yallakoraStaticData=new YallakoraStaticData();
